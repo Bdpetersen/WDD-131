c645fa4eda283e1fc9848ab3f7359fb7c28bcd61
bdpetersen <bdpetersen@pacbell.net>
2025-10-11 12:43:48 -0600

    finish pt2

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,7 +1,56 @@
-const toggleBtn = document.querySelector('.menu-toggle');
+const menuButton = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
 
-// Toggles the 'active' class on the menu when the button is clicked.
-toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('active');
+function toggleMenu() {
+  menu.classList.toggle('hide');
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
+function handleResize() {
+  if (window.innerWidth > 1000) {
+    menu.classList.remove('hide');
+  } else {
+    menu.classList.add('hide');
+  }
+}
+
+handleResize(); 
+window.addEventListener('resize', handleResize);
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('.fullview');
+
+function closeModal() {
+    modal.close();
+    modal.innerHTML = ''; 
+}
+
+function viewImage(event) {
+  const imgElement = event.target.closest('img'); 
+  
+  if (imgElement) {
+    const src = imgElement.getAttribute('src');
+    const alt = imgElement.getAttribute('alt');
+
+    const fullSrc = src.split('-')[0] + '-full.jpeg'; 
+
+    modal.innerHTML = `
+      <img src="${fullSrc}" alt="${alt}">
+      <button class='close-viewer' aria-label="Close image viewer">X</button>
+    `;
+
+    const closeButton = modal.querySelector('.close-viewer');
+    closeButton.addEventListener('click', closeModal);
+
+    modal.showModal();
+  }
+}
+
+gallery.addEventListener('click', viewImage);
+
+modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    closeModal();
+  }
 });
